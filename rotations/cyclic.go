2a4a39c578839e71fae959f0c1d79009df02e4be
Bruce Ediger <bediger8@gmail.com>
2020-04-20 09:02:47 -0600

    Remove useless hops var and counting hops

--- rotations/cyclic.go
+++ rotations/cyclic.go
@@ -1,7 +1,5 @@
 package rotations
 
-import "fmt"
-
 // Cyclic rotates an array k items to the right by jumping each item k
 // to the right, then jumping the item at k-to-the-right the next
 // iteration. The big problem with this is figuring out when to
@@ -13,23 +11,18 @@ func Cyclic(array []string, k int) (swaps int) {
 	// items if you start at i in the array, and how many times
 	// to start at successive values of i
 
-	hops := n
 	max := gcd(n, k)
 
 	// n a multiple of k: incrementing some index i by k
 	// enough times and you'll get back to i, because we're
-	// doing (i + m*k)%n to wrap the index around the array end
+	// doing (i + m*k)%n to wrap the index around the array end.
 	if n%k == 0 {
-		hops = n / k
 		max = k
 	}
-	fmt.Printf("Array length n %d, k %d, hops %d, max %d\n",
-		n, k, hops, max)
 
 	for i := 0; i < max; i++ {
 		holder := array[i]
-		for j, c := i, 0; c < hops; c++ {
-			fmt.Printf("j %d, c %d\n", j, c)
+		for j := i; true; {
 			nxt := (j + k) % n
 			tmp := array[nxt]
 			array[nxt] = holder
@@ -37,7 +30,6 @@ func Cyclic(array []string, k int) (swaps int) {
 			holder = tmp
 			j = nxt
 			if j == i {
-				fmt.Printf("j == %d, break\n", i)
 				break
 			}
 		}
